12e43ff7c1e1418ffbbdb1889805d916a59fb192
Montaser <abdelrahmanmontaser12345@gmail.com>
2021-08-05 15:55:23 +0200

    Feat(enhancement): routes enhancement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -55,7 +55,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      scrollPositionRestoration: 'top',
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
